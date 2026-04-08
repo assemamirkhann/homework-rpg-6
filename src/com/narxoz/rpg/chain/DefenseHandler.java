b28456fe4d8ee3f3b05959fb959e0cc90a30f57c
assemamirkhann <151700427+assemamirkhann@users.noreply.github.com>
2026-04-08 09:53:50 +0500

    completed passToNext logic in DefenseHandler to apply remaining damage

--- src/com/narxoz/rpg/chain/DefenseHandler.java
+++ src/com/narxoz/rpg/chain/DefenseHandler.java
@@ -20,8 +20,10 @@ public abstract class DefenseHandler {
     protected void passToNext(int damage, ArenaFighter target) {
         // TODO: If damage is still greater than 0 and a next handler exists, forward it.
         // TODO: What should happen when there is no next handler but damage is nonzero?
-        if (next != null) {
+        if (damage > 0 && next != null) {
             next.handle(damage, target);
+        } else if (damage > 0) {
+            target.takeDamage(damage);
         }
     }
 
